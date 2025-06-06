84773812867d39f44b9e371f8e19deee66f93974
Yusuke <yusuke@example.com>
2025-06-06 11:08:23 +0900

    Update alarm sound to local MP3 file

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ class PomodoroTimer {
 
 // アラーム音を追加
 const audio = new Audio();
-audio.src = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+audio.src = 'Censor_Bleep-Synth03-1(Short).mp3';
 audio.loop = false;
 document.getElementById('alarm').src = audio.src;
 
